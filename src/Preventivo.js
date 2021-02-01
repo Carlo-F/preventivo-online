47353cf7a6825a9330ffd28d740f2c9615a8d175
Carlo <carlofeniello@hotmail.it>
2021-02-01 15:49:13 +0100

    Implementata somma del totale preventivo

--- src/Preventivo.js
+++ src/Preventivo.js
@@ -5,6 +5,7 @@ class Preventivo extends React.Component {
       super(props);
       this.state = {
         stileDesign: 10,
+        numeroPagine: 10,
         contenuti: 0,
       };
   
@@ -24,28 +25,45 @@ class Preventivo extends React.Component {
     }
   
     render() {
-  
-      const totale = this.state.stileDesign + this.state.contenuti;
+
+      // const totale = this.state.stileDesign + this.state.numeroPagine + this.state.contenuti;
+      let totale = 0;
+
+      for (let key in this.state) {
+        totale += this.state[key];
+      }
   
       return (
         <form onSubmit={this.handleSubmit}>
           <div className="Section">
             <h2>Sezione 1: Design</h2>
-            <label>Stile di design</label>
-            <select name="stileDesign" value={this.state.stileDesign} onChange={this.handleChange}>
-              <option value="10">Basilare</option>
-              <option value="20">Semplice ma attrattivo</option>
-            </select>
+            <div className="Option">
+              <label>Stile di design</label>
+              <select name="stileDesign" value={this.state.stileDesign} onChange={this.handleChange}>
+                <option value="10">Basilare</option>
+                <option value="20">Semplice ma attrattivo</option>
+              </select>
+            </div>
+            <div className="Option">
+              <label>Numero di pagine</label>
+              <select name="numeroPagine" value={this.state.numeroPagine} onChange={this.handleChange}>
+                <option value="10">1-5</option>
+                <option value="20">6-10</option>
+                <option value="30">11+</option>
+              </select>
+            </div>
           </div>
           <div className="Section">
             <h2>Sezione 2: Contenuti</h2>
-            <input type="radio" id="si" name="contenuti" value="0" defaultChecked onChange={this.handleChange}/>
-            <label for="si">Ho gia'  i contenuti</label>
-            <input type="radio" id="no" name="contenuti" value="10" onChange={this.handleChange}/>
-            <label for="no">Mi servono anche i contenuti</label>
+            <div className="Option">
+              <input type="radio" id="si" name="contenuti" value="0" defaultChecked onChange={this.handleChange}/>
+              <label for="si">Ho gia'  i contenuti</label>
+              <input type="radio" id="no" name="contenuti" value="10" onChange={this.handleChange}/>
+              <label for="no">Mi servono anche i contenuti</label>
+            </div>
           </div>
           <div className="Section">
-            <h4>Totale preventivo: {totale}</h4>
+            <h2>Totale preventivo: {totale}</h2>
           </div>
         </form>
       )
