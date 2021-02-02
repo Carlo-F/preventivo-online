fb7a9f91ac29a06f33d4376a9470e745efd1097f
Carlo <carlofeniello@hotmail.it>
2021-02-02 13:54:14 +0100

    Migliorata grafica

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import './App.css';
 import Preventivo from './Preventivo';
 
@@ -7,13 +8,19 @@ class App extends React.Component {
       return (
         <div className="App">
           <header className="App-header">
-            <div>
-                <h1>Titolo</h1>
-                <p>Sottotitolo</p>
+            <div className="Section title">
+                <h1>Preventivo online per siti internet</h1>
+                <p>Calcola il preventivo per il tuo <strong>sito internet</strong> gratuitamente.<br></br>Seleziona le opzioni che ti interessano per un preventivo istantaneo.</p>
             </div>
             <Preventivo></Preventivo>
+            <div className="Section cta">
+              <p><Button className="primary" size="large" variant="contained" color="primary" href="mailto:info@carlof.it?subject= Preventivo online">✉️ Contattaci subito</Button><br></br> per confermare il preventivo</p>
+            </div>
             <div className="Section">
-            <p>Note: </p>
+            <p className="Notes">🤓 Note:<br></br>Il preventivo generato e' da considerarsi una stima basata sulle informazioni fornite.<br></br>I costi effettivi potrebbero variare.</p>
+            </div>
+            <div className="Section credits">
+              <p>Strumento di preventivo online gratuito per siti internet sviluppato da <Button className="secondary" href="https://www.carlof.it" color="secondary">Carlo F. Web Solutions</Button></p>
             </div>
           </header>
         </div>
--- src/Preventivo.js
+++ src/Preventivo.js
@@ -4,10 +4,11 @@ class Preventivo extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        stileDesign: 10,
+        stileDesign: 399,
+        numeroLingue: 0,
         testi: 0,
         immagini: 0,
-        numeroPagine: 10,
+        numeroPagine: 0,
         seo: 0,
         prodotti: 0,
         blog: false,
@@ -44,15 +45,15 @@ class Preventivo extends React.Component {
 
       for (let key in this.state) {
         if (key == 'blog' && this.state['blog'] == true) {
-          totale += 20;
+          totale += 80;
         } else if (key == 'formContatti' && this.state['formContatti'] == true) {
-          totale += 20;
+          totale += 40;
         } else if (key == 'gallery' && this.state['gallery'] == true) {
           totale += 20;
         } else if (key == 'prenotazioni' && this.state['prenotazioni'] == true) {
-          totale += 20;
+          totale += 200;
         } else if (key == 'registrazioni' && this.state['registrazioni'] == true) {
-          totale += 20;
+          totale += 500;
         } else if (key == 'analytics' && this.state['analytics'] == true) {
           totale += 20;
         } else if (key == 'chat' && this.state['chat'] == true) {
@@ -71,14 +72,14 @@ class Preventivo extends React.Component {
             <li className="Option">
               <label>Stile di design</label>
               <select name="stileDesign" value={this.state.stileDesign} onChange={this.handleChange}>
-                <option value="10">Basilare</option>
-                <option value="20">Semplice ma attrattivo</option>
-                <option value="30">Professionale</option>
-                <option value="40">Totalmente personalizzato</option>
+                <option value="399">😐 Basilare</option>
+                <option value="499">😏 Semplice ma accattivante</option>
+                <option value="599">😉 Professionale</option>
+                <option value="699">😎 Totalmente personalizzato</option>
               </select>
             </li>
             <li className="Option">
-              <label>Responsive (incluso)</label>
+              <label>Responsive (incluso) ✅</label>
             </li>
             </ul>
           </div>
@@ -86,25 +87,36 @@ class Preventivo extends React.Component {
             <h2>2. Contenuti</h2>
             <ul className="List">
             <li className="Option">
+              <label>Numero di lingue</label>
+              <select name="numeroLingue" value={this.state.numeroLingue} onChange={this.handleChange}>
+                <option value="0">Una lingua</option>
+                <option value="250">2 lingue</option>
+                <option value="350">3 lingue</option>
+                <option value="450">4 lingue</option>
+                <option value="650">5 lingue +</option>
+              </select>
+            </li>
+            <li className="Option">
               <label>Hai gia' i testi?</label>
               <select name="testi" value={this.state.testi} onChange={this.handleChange}>
-                <option value="0">Si</option>
-                <option value="10">No</option>
+                <option value="0">😌 Si</option>
+                <option value="100">🥺 No</option>
               </select>
             </li>
             <li className="Option">
               <label>Hai gia' le immagini?</label>
               <select name="immagini" value={this.state.immagini} onChange={this.handleChange}>
-                <option value="0">Si</option>
-                <option value="10">No</option>
+                <option value="0">😌 Si</option>
+                <option value="100">🥺 No</option>
               </select>
             </li>
             <li className="Option">
               <label>Numero di pagine</label>
               <select name="numeroPagine" value={this.state.numeroPagine} onChange={this.handleChange}>
-                <option value="10">1-5</option>
-                <option value="20">6-10</option>
-                <option value="30">11+</option>
+                <option value="0">Pagina singola</option>
+                <option value="250">2-5</option>
+                <option value="300">6-10</option>
+                <option value="450">11+</option>
               </select>
             </li>
             </ul>
@@ -113,20 +125,20 @@ class Preventivo extends React.Component {
             <h2>3. Presenza su internet</h2>
             <ul className="List">
             <li className="Option">
-              <label>Dominio (incluso)</label>
+              <label>Dominio (incluso) ✅</label>
             </li>
             <li className="Option">
-              <label>Hosting (incluso)</label>
+              <label>Hosting (incluso) ✅</label>
             </li>
             <li className="Option">
-              <label>Certificato HTTPS (incluso)</label>
+              <label>Certificato HTTPS (incluso) ✅</label>
             </li>
             <li className="Option">
               <label>Ottimizzazione SEO</label>
               <select name="seo" value={this.state.seo} onChange={this.handleChange}>
-                <option value="10">Base</option>
-                <option value="20">Ottimizzato</option>
-                <option value="30">Professionale</option>
+                <option value="0">😐 Base</option>
+                <option value="100">😌 Ottimizzato</option>
+                <option value="300">🤩 Professionale</option>
               </select>
             </li>
             </ul>
@@ -138,9 +150,9 @@ class Preventivo extends React.Component {
               <label>Numero di prodotti</label>
               <select name="prodotti" value={this.state.prodotti} onChange={this.handleChange}>
                 <option value="0">Non ne ho bisogno</option>
-                <option value="20">1-10 prodotti</option>
-                <option value="30">11-100 prodotti</option>
-                <option value="40">100 + prodotti</option>
+                <option value="500">🛍 1-10 prodotti</option>
+                <option value="700">🛒 11-100 prodotti</option>
+                <option value="950">🏬 100 + prodotti</option>
               </select>
             </li>
             </ul>
@@ -149,37 +161,37 @@ class Preventivo extends React.Component {
             <h2>5. Opzioni aggiuntive</h2>
             <ul className="List">
               <li className="Option">
-                <label for="blog">Blog</label>
+                <label for="blog">✍️ Blog</label>
                 <input type="checkbox" id="blog" name="blog" onChange={this.handleChange}></input>
               </li>
               <li className="Option">
-                <label for="formContatti">Form di contatti</label>
+                <label for="formContatti">✉️ Form di contatti</label>
                 <input type="checkbox" id="formContatti" name="formContatti" onChange={this.handleChange}></input>
               </li>
               <li className="Option">
-                <label for="gallery">Foto/Video gallery</label>
+                <label for="gallery">📷 Foto/Video gallery</label>
                 <input type="checkbox" id="gallery" name="gallery" onChange={this.handleChange}></input>
               </li>
               <li className="Option">
-                <label for="prenotazioni">Sistema di prenotazioni</label>
+                <label for="prenotazioni">🗓 Sistema di prenotazioni</label>
                 <input type="checkbox" id="prenotazioni" name="prenotazioni" onChange={this.handleChange}></input>
               </li>
               <li className="Option">
-                <label for="registrazioni">Registrazione utenti (Registrati/Accedi)</label>
+                <label for="registrazioni">🤵🏻‍♂️ Registrazione utenti (Registrati/Accedi)</label>
                 <input type="checkbox" id="registrazioni" name="registrazioni" onChange={this.handleChange}></input>
               </li>
               <li className="Option">
-                <label for="analytics">Google Analytics</label>
+                <label for="analytics">📈 Google Analytics</label>
                 <input type="checkbox" id="analytics" name="analytics" onChange={this.handleChange}></input>
               </li>
               <li className="Option">
-                <label for="chat">Live chat</label>
+                <label for="chat">💬 Live chat</label>
                 <input type="checkbox" id="chat" name="chat" onChange={this.handleChange}></input>
               </li>
             </ul>
           </div>
           <div className="Section">
-            <h2>Totale preventivo: {totale}</h2>
+            <h2>✨ Costo stimato € {totale} ✨</h2>
           </div>
         </form>
       )
